d1646ffe75dab4d134c1c063e6be0503a50ecab3
Hassan Refaat <hassan.refaat.dev@gmail.com>
2023-08-03 08:09:44 +0300

    Initial commit

---
+++ src/test/java/com/handy/handy/HandyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handy.handy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
